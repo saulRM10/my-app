821de50cbe55cf9581019e8a70e687b50cf50d59
saulRM10 <43281985+saulRM10@users.noreply.github.com>
2022-05-03 09:59:50 -0700

    delete files from the default react app installation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+//import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -8,10 +8,12 @@ import reportWebVitals from './reportWebVitals';
 import TestComponent from './components/text';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    // <App />
-  // </React.StrictMode>
-  <TestComponent name="saul" /> 
+   <div>
+    
+   {/* <App /> */}
+  
+  <TestComponent name="saul" />
+   </div>
   
 );
 
