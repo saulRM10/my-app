830bd8042c2a1786110dcdcc570977f04ca9ed49
saulRM10 <43281985+saulRM10@users.noreply.github.com>
2022-05-03 09:39:41 -0700

    initial commit

---
+++ src/components/text.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+function TestComponent(){
+    return <h1> hello from component </h1> ; 
+}
+
+export default TestComponent; 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// import new component 
+import TestComponent from './components/text';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+    // <App />
+  // </React.StrictMode>
+  <TestComponent /> 
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
