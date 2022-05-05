ca632750c4a88ab09c15b535149e486c0c7cc222
saulRM10 <43281985+saulRM10@users.noreply.github.com>
2022-05-05 08:16:25 -0700

    adding notes about the hooks I am learning more about

--- src/components/counter.js
+++ src/components/counter.js
@@ -2,13 +2,23 @@ import React, { useEffect, useState } from "react";
 
 function Counter(){
 
-    // use effect hook 
+    // use state hook ; whenever data changes re-render UI
     const [count, setCount] = useState(0); 
-    
+    // count => reactive data /or state, (0) = default value 
+    // setCount => setter function
+
+    // useEffect
+    // component lifecycle: mounting(initialized, happens once), updating(state is updated, can happen multiple time), unmounting(destroyed)
+    // is a function that uses a function (you) I decide, it runs when mounted or when state changes
     useEffect(()=> {
         document.title = ' you clicked ${count} times '; 
-    }); 
 
+        // tear down code, will be called when component is destroyed 
+        return () => alert('goodbye component !'); 
+    }, 
+        [] // array of dependencies, usually left empty if you just want it to run once when the component is initialized
+    ); 
+ 
    function handleCount(){
         setCount(count + 1); 
     }; 
